64491416c894f01cacbae6c7f0f660e6c03284a1
surayya-lab <76586836+surayya-lab@users.noreply.github.com>
2021-06-05 20:20:52 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# zig
+# TopplingBoxes
+Toppling Boxes
---
+++ sketch.js
@@ -0,0 +1,30 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box1;
+
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    box1 = new Box(200,300,50,50);
+    box2 = new Box(240,100,50,100);
+    ground1= new Ground(300,350,190,PI/3)
+    ground2= new Ground(120,350,195,-PI/3)
+}
+
+
+function draw(){
+    background("grey");
+    Engine.update(engine);
+    console.log(box2.body.position.x);
+    console.log(box2.body.position.y);
+    console.log(box2.body.angle);
+    box1.display();
+    box2.display();
+    ground1.display();
+    ground2.display();
+}
